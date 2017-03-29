a7ce467977b549cd5abae5eddc6eaa62d0b6b35a
Hooooolly <Holly@Jiayis-MacBook-Pro.local>
2017-03-29 15:18:44 -0500

    Add removeItemTest; index for remove; and speechAssets
    Three tests in removeItemTest:
    1. remove item that is there
    2. remove item that is not there
    3. invalid intput
    
    Update index and SampleUtterances for RemoveItem part

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const Alexa = require('alexa-sdk');
 const dynasty = require('dynasty')({});
 const languageStrings = require('./languageStrings');
 const addItem = require('./intentDelegates/addItem');
+const removeItem = require('./intentDelegates/removeItem');
 
 const APP_ID = "amzn1.ask.skill.8371afd6-d231-4b54-bf1d-5987733228cd";
 const stewardItems = dynasty.table('Steward_Items');
@@ -11,6 +12,8 @@ const stewardItems = dynasty.table('Steward_Items');
 const handlers = {
 
     'AddItem': function() { addItem(this, stewardItems) },
+
+    'RemoveItem': function() { removeItem(this, stewardItems) },
 
     'Affirmative': function() {
         const responses = this.t('AFFIRMATIVE_MESSAGE');
