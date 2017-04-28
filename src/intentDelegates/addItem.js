8ad8f311f9b0e9944021c79ba5fffe8f0e5346f9
TEWatson <taylorwatson6895@yahoo.com>
2017-04-28 14:47:34 -0500

    Fixed AddItem so it works by logic rather than asynchronous black magic

--- src/intentDelegates/addItem.js
+++ src/intentDelegates/addItem.js
@@ -6,7 +6,6 @@ module.exports = function(handler, table) {
 
     const slots = handler.event.request.intent.slots
     var setQuantity = 1;
-    var skip = false;
 
     if (slots.Item && slots.Item.value) {
         table
@@ -16,24 +15,21 @@ module.exports = function(handler, table) {
         .then(function(resp) {
             if (resp == undefined) {
               createItem(handler, table);
-              skip = true;
+            } else {
+              if (slots.Quantity && slots.Quantity.value) {
+                setQuantity = slots.Quantity.value;
+              }
+              table
+              .update({hash: handler.event.session.user.userId, range: slots.Item.value.toLowerCase()}, { quantity: setQuantity })
+              .then(function(resp) {
+                  handler.emit('Affirmative');
+              })
+              .catch(function(err) {
+                  console.log(err);
+                  handler.emit('Error');
+              });
             }
         })
-
-      if (!skip) {
-        if (slots.Quantity && slots.Quantity.value) {
-          setQuantity = slots.Quantity.value;
-        }
-        table
-        .update({hash: handler.event.session.user.userId, range: slots.Item.value.toLowerCase()}, { quantity: setQuantity })
-        .then(function(resp) {
-            handler.emit('Affirmative');
-        })
-        .catch(function(err) {
-            console.log(err);
-            handler.emit('Error');
-        });
-      }
 
     } else {
         console.log("error with itemName slot");
