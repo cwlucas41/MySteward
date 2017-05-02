6c94ba561a58bb81c56551bff4e721a427b1d37a
TEWatson <taylorwatson6895@yahoo.com>
2017-05-02 17:48:09 -0500

    First version of queryItemThreshold

---
+++ src/intentDelegates/queryQuantityThreshold.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = function(handler, table) {
+
+    const slots = handler.event.request.intent.slots
+    var returnedItems = [];
+
+    if (slots.Quantity && slots.Quantity.value) {
+        table
+            .findAll(handler.event.session.user.userId)
+            .then(function(userItems) {
+                if (userItems != undefined) {
+                    userItems.forEach(function(item) {
+                        if (item.quantity < slots.Quantity.value) {
+                            returnedItems.push(item.itemName) // pluralize this when emitting for more natural response
+                        }
+                    });
+                    handler.emit(':tell', handler.t('THRESHOLD_GROUP', slots.Quantity.value, parseItemsIntoString(returnedItems)));
+                }
+                else {
+                  handler.emit(':tell', handler.t('THRESHOLD_NONE', slots.Quantity.value))
+                }
+            })
+            .catch(function(err) {
+                console.log(err);
+                handler.emit('Error');
+            });
+
+    } else {
+        console.log("error with itemName slot");
+        handler.emit('Error')
+    }
+}
